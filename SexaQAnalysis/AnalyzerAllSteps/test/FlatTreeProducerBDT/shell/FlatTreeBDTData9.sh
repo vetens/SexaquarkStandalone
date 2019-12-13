b59cd2ba9bcd01485864e310f9c22fc840b2dbc4
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-12-13 11:23:35 +0100

    input files for FlatTreeBDT

---
+++ SexaQAnalysis/AnalyzerAllSteps/test/FlatTreeProducerBDT/shell/FlatTreeBDTData9.sh
@@ -0,0 +1,8 @@
+source $VO_CMS_SW_DIR/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc481
+cd /user/jdeclerc/CMSSW_8_0_30_bis/src ; eval `scram runtime -sh` ; cd - >/dev/null
+export X509_USER_PROXY=/user/$USER/x509up_u$(id -u $USER)
+export X509_USER_PROXY=/tmp/x509up_u20641
+voms-proxy-init -pwstdin < /storage_mnt/storage/user/jdeclerc/CMSSW_8_0_30/src/STEP1_STEP2_Skimming_FlatTree/qsub/password
+cd /user/jdeclerc/CMSSW_8_0_30_bis/src/SexaQAnalysis/AnalyzerAllSteps/test/FlatTreeProducerBDT
+cmsRun /user/jdeclerc/CMSSW_8_0_30_bis/src/SexaQAnalysis/AnalyzerAllSteps/test/FlatTreeProducerBDT/FlatTreeProducerBDT_cfg.py print inputFiles_load=/user/jdeclerc/CMSSW_8_0_30_bis/src/SexaQAnalysis/AnalyzerAllSteps/test/FlatTreeProducerBDT/inputFilesLists/inputFiles_FlatTreeProducerBDT_SinglePhoton_Run2016G-07Aug17-v1_trialR.txt outputFile=file:/user/jdeclerc/CMSSW_8_0_30_bis/src/SexaQAnalysis/AnalyzerAllSteps/test/FlatTreeProducerBDT/Results/FlatTreeBDT_SinglePhoton_Run2016G-07Aug17-v1_trialR.root
